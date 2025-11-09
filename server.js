436f9af537bcf2d97eaadf1705e3e90f283ac5b2
Paola Novick <paola.novick@davinci.edu.ar>
2025-11-09 11:54:12 -0300

    Fix: habilitar CORS solo para Vercel

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ const app = express();
 app.use(
   cors({
     origin: "https://bebidas-delivery-api.vercel.app",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type, Authorization",
     credentials: true,
   })
 );
